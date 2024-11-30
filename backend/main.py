b0b2e6bae39009786f7082070a99251cce589997
Pratik Paudel <56014021+PratikPaudel@users.noreply.github.com>
2024-11-30 10:57:13 -0600

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -20,7 +20,7 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:5173",  # Local development
         "https://git-nest-pratikpaudels-projects.vercel.app",
-        "https://git-nest.vercel.app/"
+        "https://git-nest.vercel.app"
     ],
     allow_credentials=True,
     allow_methods=["*"],
